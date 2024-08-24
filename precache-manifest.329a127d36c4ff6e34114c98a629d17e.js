5f2e25eb9acb1b3c8dea0c22f4d1271e36941e71
PatekBosco <workspacese@gmail.com>
2024-08-24 19:52:12 +0300

    Add files via upload

---
+++ precache-manifest.329a127d36c4ff6e34114c98a629d17e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997e52654c689cb1e0c801c4273598b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20dad713e34dc4327c88",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "49d53756381de06665bb",
+    "url": "/static/js/2.ba7e1342.chunk.js"
+  },
+  {
+    "revision": "20dad713e34dc4327c88",
+    "url": "/static/js/main.094b346f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
